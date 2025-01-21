6707b801da38ed635b1948a8cd39b711f53f143e
xuancat0208 <xuancat0208@gmail.com>
2025-01-21 16:08:54 +0800

    優化 BukkitRecipe#creates 邏輯

--- src/main/java/com/aeltumn/autocraft/impl/BukkitRecipe.java
+++ src/main/java/com/aeltumn/autocraft/impl/BukkitRecipe.java
@@ -203,7 +203,7 @@ public class BukkitRecipe implements CraftingRecipe {
 
     @Override
     public boolean creates(ItemStack stack) {
-        var clone = stack.clone();
+        /*var clone = stack.clone();
 
         // For all block state meta items we clear the block entity tag off the item we use for comparisons
         // so a full shulker box is accepted as craftable
@@ -218,7 +218,25 @@ public class BukkitRecipe implements CraftingRecipe {
             clone.setItemMeta(meta);
         }
 
-        return Utils.isSimilar(result, clone);
+        return Utils.isSimilar(result, clone);*/
+        // xuancat 這裡可以改成如果有 blockEntityTag 再執行複製
+        if (stack.hasItemMeta() && blockEntityTag != null && stack.getItemMeta() instanceof BlockStateMeta meta) {
+            return Optional.of(new ItemStack(stack.getType(), stack.getAmount()))
+                    .filter(clone -> {
+                        try {
+                            blockEntityTag.set(meta, null);
+                            return true;
+                        } catch (Exception exception) {
+                            exception.printStackTrace();
+                            return false;
+                        }
+                    })
+                    .filter(clone -> clone.setItemMeta(meta))
+                    .map(clone -> Utils.isSimilar(result, clone))
+                    .orElse(false);
+        } else {
+            return Utils.isSimilar(result, stack);
+        }
     }
 
     @Override
